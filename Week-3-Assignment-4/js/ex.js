fd9d854cf12cfbe0c4557e115aead5a7241dec70
Matt <mattsaab@Matts-MacBook-Pro-2.local>
2015-09-28 19:30:30 -0400

    Add index.html

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,28 @@
-// JavaScript Document
+var boxOne = null;
+var boxTwo = null;
+
+document.addEventListener("DOMContentLoaded", function(){
+    boxOne = document.querySelector(".long-rect");
+	boxTwo = document.querySelector(".short-rect");
+	boxOne.addEventListener("mouseover", myFunction);
+	
+});	
+
+function myFunction() {
+	boxTwo.innerText="Mouse over";
+	boxTwo = document.querySelector(".short-rect");
+	boxOne.addEventListener("mouseout", functionTwo);
+	}
+	
+function functionTwo() {
+	boxTwo = document.querySelector(".short-rect")
+	boxTwo.innerText= "Mouse out";
+	boxTwo.addEventListener("click", functionThree);
+}
+
+function functionThree() {
+	alert("clicked");
+	
+}// JavaScript Document
 
 
